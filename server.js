fcb5a6ee588e1f125d173e41ef0730ee9fe5a5f6
Matt Pindell <pindell.m@gmail.com>
2016-07-11 11:49:34 -0600

    Basic post and get complete

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const generateId = require('./lib/generate-id');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('static'));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pizza Express';
+app.locals.pizzas = {};
+
+app.set('view engine', 'jade');
 
 app.get('/', (request, response) => {
-  response.send('Hello World!');
+  response.render('index');
 });
 
-// app.listen(app.get('port'), () => {
-//   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-// });
+app.post('/pizzas', (request, response) => {
+  var id = generateId();
+
+  app.locals.pizzas[id] = request.body;
+
+  response.sendStatus(201);
+});
+
+app.get('/pizzas/:id', (request, response) => {
+  var pizza = app.locals.pizzas[request.params.id];
+
+  response.render('pizza', { pizza: pizza });
+});
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {
