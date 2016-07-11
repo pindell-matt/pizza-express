ae6e0832c78b4008ab81d9c4b8900598c3a60a74
Matt Pindell <pindell.m@gmail.com>
2016-07-11 09:35:07 -0600

    Create test directory

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get('/', (request, response) => {
   response.send('Hello World!');
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-});
+// app.listen(app.get('port'), () => {
+//   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+// });
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+module.exports = app;
